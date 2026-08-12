41f6a802d0a4ce2ffd3f64b2715b78c2067b297a
Hack1me <sahitndaheu@gmail.com>
2026-08-12 03:27:55 +0100

    correcting sign_up page

--- acount/views.py
+++ acount/views.py
@@ -1,18 +1,21 @@
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import redirect, render
 
 
-# Create your views here.
+# sign up view
 def sign_up(request):
     if request.method == 'POST':
+        
         # Handle form submission here
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password1')
         confirm_password = request.POST.get('password2')
         
+        # Validate email format
         error = False
         messages = []
         try:
@@ -22,16 +25,30 @@ def sign_up(request):
             messages.append("Invalid email address.")
             error = True
             return render(request, 'acount/sign_up.html', {'messages': messages})
+        
+        # Validate password and confirm password match
         if not error and password != confirm_password:
                 # Handle password mismatch
             messages.append("Passwords do not match.")
             error = True
             return render(request, 'acount/sign_up.html', {'messages': messages})
-            
+        
+        # Check if the username or email already exists
+        user = User.objects.filter(Q(username=username) | Q(email=email)).first()
+        if user :
+            messages.append(f"Username {username} or email {email} already exists.")
+            error = True
+            return render(request, 'acount/sign_up.html', {'messages': messages})
+        
+        # Create the user if there are no errors       
         if not error:
-            print(f"Username: {username}, Email: {email}, Password: {password}, Confirm Password: {confirm_password}")
-            messages.append("Account created successfully.")
-            return render(request, 'acount/sign_in.html', {'success': True})
+            user = User(
+                username=username, 
+                email=email
+                )
+            user.set_password(password)
+            user.save()
+            return redirect('sign_in')
     else:
         return render(request, 'acount/sign_up.html')
 
